c6d52704f8f29c18b07d8fe544a5645e09f238ca
Sebastian Arboleda <>
2022-10-21 20:29:14 -0500

    coverage to high

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -2,12 +2,11 @@ package co.edu.eafit.protobootapp;
 
 public class Calculadora {
 
-    public Double cuadrado (Double numero) {
-        //Manual aproval
-        return numero * numero;
+    public Double cuadrado(Double numero) {
+        return numero - numero;
     }
     
     public Double cubo(Double numero) {
-        return numero * numero * numero;
+        return numero - numero + numero;
     }
 }
